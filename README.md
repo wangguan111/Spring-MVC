028a6fd6117c0ba8e47c512c87e31d81c72b3162
wangguan111 <wang_guan111@126.com>
2018-06-08 23:38:58 +0800

    spring framework

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Spring-MVC
 learn and pratice the book &lt;&lt;Spring MVC: A Tutorial (Second Edition)>>
+
+Chapter 1: The Spring Framework
---
+++ chapter-01/spring-intro/src/main/java/springintro/Main.java
@@ -0,0 +1,39 @@
+package springintro;
+
+import java.time.LocalDate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import springintro.bean.Employee;
+import springintro.bean.Product;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"spring-config.xml"});
+        
+        Product product1 = context.getBean("product", Product.class);
+        product1.setName("Excellent snake oil");
+        System.out.println("product1: " + product1.getName());
+        
+        Product product2 = context.getBean("product", Product.class);
+        System.out.println("product2: " + product2.getName());
+        
+        LocalDate localDate = context.getBean("localDate", java.time.LocalDate.class);
+        System.out.println("today:" + localDate);
+        
+        Product featuredProduct = context.getBean("featuredProduct", Product.class);
+        System.out.println(featuredProduct.getName() + ", " + featuredProduct.getDescription() + ", " + featuredProduct.getPrice());
+        
+        Product featuredProduct2 = context.getBean("featuredProduct2", Product.class);
+        System.out.println(featuredProduct2.getName() + ", " + featuredProduct2.getDescription() + ", " + featuredProduct2.getPrice());
+             
+        Employee employee1 = context.getBean("employee1", Employee.class);
+        System.out.println(employee1.getFirstName() + " " + employee1.getLastName());
+        System.out.println(employee1.getHomeAddress());
+
+        Employee employee2 = context.getBean("employee2", Employee.class);
+        System.out.println(employee2.getFirstName() + " " + employee2.getLastName());
+        System.out.println(employee2.getHomeAddress());  
+    }
+}
